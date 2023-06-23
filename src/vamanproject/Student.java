b8931c3df705deb5515dc999ba8615244eba5e64
rohit-narain <rohitnarain@gmail.com>
2023-06-23 10:36:24 +0000

    Create Student.java

---
+++ src/vamanproject/Student.java
@@ -0,0 +1,17 @@
+package vamanproject;
+
+import java.lang.reflect.Method;
+
+public class Student {
+ String name;
+ int age;
+ String address;
+ float percentage;
+ 
+
+  static boolean isScholer(float percentage) {
+	  percentage >= 85.00
+			  };
+ 
+ 
+}
